101eb173081edb9b50396dbbeeeb9c91671b1cea
Arstan Abylov <abylovarstan7@gmail.com>
2022-03-02 14:50:11 +0600

    added 3 fails

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.accordion').forEach(accordion => {
+    accordion.querySelector('.accordion__header').addEventListener('click', function() {
+        accordion.classList.toggle('accordion--open');
+    });
+});
